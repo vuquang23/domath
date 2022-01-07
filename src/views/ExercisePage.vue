51c67fe70cce51b3f5562ced65d875782ec30826
ngot23 <levuquang2k1@gmail.com>
2022-01-07 19:07:49 +0700

    (+) update quiz

--- src/views/ExercisePage.vue
+++ src/views/ExercisePage.vue
@@ -15,7 +15,7 @@
               <h1 class="fw-bold" style="color: #1ab394">{{ quizz["heading"] }}</h1>
             </div>
             <div style="margin-top: 2em; margin-bottom: 2em">
-            <iframe width="600" height="320" :src="quizz['tutorial']['src']" allow="autoplay"></iframe>
+            <iframe class="ratio ratio-16x9 embeded-vid" width="620" height="380" :src="quizz['tutorial']['src']" allow="autoplay"></iframe>
             </div>
             <button class="button-74" @click="page = page + 1">Làm bài</button>
           </div>
@@ -36,6 +36,7 @@
             <div>
               <p v-if="quizz[`quiz-${page}`]['question']['type']==='img'||quizz[`quiz-${page}`]['question']['type']==='video'" class="text-start fw-bold">Em hãy trả lời câu hỏi trong ảnh/video bên dưới</p>
               <iframe v-if="quizz[`quiz-${page}`]['question']['type']==='video'" 
+                class="ratio embeded-vid"
                 width="600" height="300" 
                 :src="quizz[`quiz-${page}`]['question']['src']" 
                 allow="autoplay"
@@ -118,7 +119,7 @@
                 <p class="text-start fw-bold" @click="viewSolution = viewSolution ^ 1" style="color: #1ab394; cursor: pointer;">Xem cách giải</p>
               </div>
               <div v-if="viewSolution" style="margin-top: 0;">
-                <iframe width="600" height="300" :src="quizz[`quiz-${page}`]['solution']['src']" allow="autoplay"></iframe>
+                <iframe class="ratio embeded-vid" width="600" height="300" :src="quizz[`quiz-${page}`]['solution']['src']" allow="autoplay"></iframe>
               </div>
             </div>
           </div>
@@ -250,6 +251,11 @@ export default {
 
 .picture-answer {
   width: 5.8em;
+}
+
+.embeded-vid {
+  padding-left: 1em;
+  padding-right: 1em;
 }
 
 .button-53 { background-color: #3DD1E7; border: 0 solid #E5E7EB; box-sizing: border-box; color: #000000; display: flex; font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"; font-size: 1rem; font-weight: 700; justify-content: center; line-height: 1.75rem; padding: .75rem 1.65rem; position: relative; text-align: center; text-decoration: none #000000 solid; text-decoration-thickness: auto; width: 100%; max-width: 460px; position: relative; cursor: pointer; transform: rotate(-2deg); user-select: none; -webkit-user-select: none; touch-action: manipulation; } .button-53:focus { outline: 0; } .button-53:after { content: ''; position: absolute; border: 1px solid #000000; bottom: 4px; left: 4px; width: calc(100% - 1px); height: calc(100% - 1px); } .button-53:hover:after { bottom: 2px; left: 2px; } @media (min-width: 768px) { .button-53 { padding: .75rem 3rem; font-size: 1.25rem; } }
